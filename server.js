41157d045622b65a3a5b8d1ccd7c0fe9f3edd9cc
Rob Terrell <Rterrell25@gmail.com>
2019-10-26 11:52:07 -0400

    yelp api server mod

--- server.js
+++ server.js
@@ -5,23 +5,26 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require("express")
 const path = require("path")
 const app = express()
+const axios = require("axios")
 
-// JUST FOR DEMO PURPOSES, PUT YOUR ACTUAL API CODE HERE
-app.get('/api/demo', (request, response) => {
-  response.json({
-    message: "Hello from server.js"
+app.get('/api/bars/search/:location', (request, response) => {
+  const { location } = request.params
+
+  axios.get(`https://api.yelp.com/v3/businesses/search?term=bars&location=${location}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.YELP_API_KEY}`
+    }
   })
+  .then(yelpResponse => {
+    const bars = yelpResponse.data.businesses || []
+    response.json(bars)
+  })
+
+
 })
-// END DEMO
 
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')))
-  // Handle React routing, return all requests to React app
-  app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-  })
-}
+
+
 
 const port = process.env.PORT || 8080
 app.listen(
